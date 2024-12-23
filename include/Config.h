7fe01e5153dceda9087b193e31faf43e3a272c76
Anant Sharma <anantsh.88@gmail.com>
2024-12-23 18:25:36 +0530

    feat: add basic project setup
    Structure needs more thought, a better system is possible

---
+++ include/Config.h
@@ -0,0 +1,2 @@
+#define Lockey_VERSION_MAJOR 0
+#define Lockey_VERSION_MINOR 1
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include "Config.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    cout << argv[0] << " Version " << Lockey_VERSION_MAJOR << "."
+         << Lockey_VERSION_MINOR << endl;
+  }
+  return 0;
+}
